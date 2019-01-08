a16084df05b6a7bf2913f64898d51196252763e5
Mari Teiler-Johnsen <mari.teiler.johnsen@gmail.com>
2019-01-08 13:55:39 +0100

    Improving variable declarations

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
 // @flow
 /* eslint eqeqeq: "off" */
-var express = require("express");
-var mysql = require("mysql");
-var app = express();
-var bodyParser = require("body-parser");
-var jwt = require("jsonwebtoken");
-var cors = require("cors");
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json()); // for å tolke JSON
 app.use(function(req, res, next) {
@@ -22,9 +22,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
-const ArtikkelDao = require("../dao/hverdagsdao.js");
+const Hverdagsdao = require("../dao/hverdagsdao.js");
 
-var pool = mysql.createPool({
+const pool = mysql.createPool({
   connectionLimit: 10,
   host: "mysql.stud.iie.ntnu.no",
   user: "mathibra",
@@ -33,7 +33,7 @@ var pool = mysql.createPool({
   debug: false
 });
 
-let artikkelDao = new ArtikkelDao(pool);
+let artikkelDao = new Hverdagsdao(pool);
 
 app.get("/cases", (req, res) => {
   console.log("/cases fikk request.");
@@ -44,7 +44,7 @@ app.get("/cases", (req, res) => {
 });
 
 
-var server = app.listen(process.env.PORT || "8080", function() {
+const server = app.listen(process.env.PORT || "8080", function() {
   console.log("App listening on port %s", server.address().port);
   console.log("Press Ctrl+C to quit");
 });
