0ff9918ec252c7d5dd84cb2c41d7c51fdab3a2d2
atdbui <atdbui@stud.ntnu.no>
2019-01-09 14:09:22 +0100

    Remember to  npm install --save react-mdl and

--- client/src/index.js
+++ client/src/index.js
@@ -2,26 +2,18 @@
 /* eslint eqeqeq: "off" */
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-import { Switch, Route } from "react-router-dom";
-import { HashRouter, Router, NavLink } from 'react-router-dom';
-import "./style.css";
-import createHashHistory from "history/createHashHistory";
-import Cases from "./Cases";
-import CasePage from "./Components/CasePage";
-import UserHome from "./Components/userHome";
-import Events from "./Components/events";
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-
 import { HashRouter, Route, NavLink } from 'react-router-dom';
 import "./style.css";
 import createHashHistory from "history/createHashHistory";
 import Cases from "./Cases";
-import CasePage from "./components/CasePage";
-import CaseListCard from "./components/CaseListCard";
-import ProfileCard from "./components/ProfileCard";
-import ProfilePage from "./components/ProfilePage";
+import CasePage from "./Components/CasePage";
+import CaseListCard from "./Components/CaseListCard";
+import ProfileCard from "./Components/ProfileCard";
+import ProfilePage from "./Components/ProfilePage";
+import UserHome from "./Components/userHome";
+import Events from "./Components/events";
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 
 const history = createHashHistory();
@@ -117,25 +109,6 @@ ReactDOM.render(<Main />, document.getElementById("root"));
 const root = document.getElementById("root");
 
 
-const root = document.getElementById('root');
-
-function renderRoot(){
-    if (root)
-        ReactDOM.render(
-            <HashRouter>
-                <div id='page'>
-                    <Navbar />
-                    {/*<Route exact path='/' component={Home} />*/}
-                    {/*<Route exact path='/register' component={Register} />*/}
-                    {/*<Route exact path='/article/:id' component={ArticleInfo} />*/}
-                    {/*<Route exact path='/article/:id/edit' component={ArticleEdit} />*/}
-                    <Route exact path='/userHome' component={UserHome}/>
-                       <Route exact path='/events' component={Events}/>
-                </div>
-            </HashRouter>,
-            root
-        );
-
 function renderRoot() {
   if (root)
     ReactDOM.render(
@@ -144,6 +117,8 @@ function renderRoot() {
           <Navbar />
           <Route exact path="/" component={Menu} />
           <Route exact path="/CasePage" component={CasePage} />
+          <Route exact path='/userHome' component={UserHome}/>
+          <Route exact path='/events' component={Events}/>
         {/*<Route exact path="/CaseListCard" component={IssueOverview} />*/}
         </div>
       </HashRouter>,
