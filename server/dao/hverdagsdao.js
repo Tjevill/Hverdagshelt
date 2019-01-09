2b5aaf17c0c3a3ca48d9df700f4af3cca4a7701f
Ben <benos@stud.ntnu.no>
2019-01-09 10:11:34 +0100

    ufungerende post endringer

--- server/dao/hverdagsdao.js
+++ server/dao/hverdagsdao.js
@@ -1,6 +1,6 @@
 const Dao = require("./dao.js");
 
-module.exports = class ArtikkelDao extends Dao {
+module.exports = class hverdagsdao extends Dao {
   getAllCases(callback) {
     super.query(
       "select * from Cases",
@@ -8,4 +8,15 @@ module.exports = class ArtikkelDao extends Dao {
       callback
     );
   }
+
+  createCase(case: Object, callback: any) {
+
+    let val = [case.description, case.longitude, case.latitude, case.status_id, case.user_id, case.category_id, case.zipcode]
+    super.query(
+      "INSERT INTO Cases (description, longitude, latitude, status_id, user_id, category_id, zipcode) values (?,?,?,?,?,?,?)",
+      val,
+      callback
+    );
+  }
+
 };
--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const nodemailer = require('nodemailer');
 app.use(cors());
 app.use(bodyParser.json()); // for å tolke JSON
 
@@ -36,7 +37,25 @@ const pool = mysql.createPool({
   debug: false
 });
 
+// Authentication with bedrehverdagshelt@gmail.com
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'bedrehverdagshelt@gmail.com',
+    pass: 'JegErDinHelt69'
+  }
+});
+
 let hverdagsdao = new Hverdagsdao(pool);
+
+/*
+REST-Architecture:
+/cases
+POST: Create new case
+GET: get all cases
+
+
+*/
 
 app.get("/cases", (req, res) => {
   console.log("/cases fikk request.");
@@ -45,6 +64,40 @@ app.get("/cases", (req, res) => {
     res.json(data);
   });
 });
+
+app.post("/cases", (req: Object, res: Response) => {
+  console.log("/cases fikk POST request");
+
+  if(!req.body) {
+    return res.sendStatus(400);
+  }else {
+    hverdagsdao.createCase({
+    description: req.body.description,
+    longitude: req.body.longitude,
+    latitude: req.body.latitude,
+    status_id: req.body.status_id,
+    user_id: req.body.user_id,
+    category_id: req.body.category_id,
+    zipcode: req.body.zipcode
+    }
+    ,(status, data) => {
+    res.status(status); 
+    res.json(data);
+    
+
+  });
+ 
+
+  
+
+  transporter.sendMail(mailOptionsCase, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+    });
+}});
 
 
 const server = app.listen(process.env.PORT || "8080", function() {
