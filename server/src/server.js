829070dd1500b5db6e44cb403839d5d09e22d866
Øyvind Valstadsve <oyvinval@stud.ntnu.no>
2019-01-10 11:09:49 +0100

    Made new dao for employees

--- server/src/server.js
+++ server/src/server.js
@@ -357,13 +357,10 @@ app.get("/eventOnDateAsc/:date", (req, res) => {
         });
     });
 
-<<<<<<< HEAD
+
   //End Case
 
 
-
-=======
->>>>>>> c40e01c4415b528c19c3bd3dbba2baef75939cc7
 app.post("/cases", (req, res) => {
   console.log("/cases received POST-request");
   console.log(req.body.description);
