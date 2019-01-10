d489f39d030b0d67b48c02d2618d966ee74b7ac9
erlinrol <erlinrol@stud.ntnu.no>
2019-01-10 12:29:49 +0100

    Added mapservice

--- client/src/services.js
+++ client/src/services.js
@@ -208,62 +208,61 @@ class UserService {
   getCountUsers(): Promise<number>{
     return axios.put(url + '/userCount');
   }
-  
+
   getEmailUserByID(id: number): Promise<string>{
     return axios.get(url + '/userEmail/' + id);
   }
-  
+
   updateSubscription(userSubUpdate: UserSubscriptionUpdate): Promise<void>{
     return axios.put(url + '/userSubscriptionUpdate', userSubUpdate);
   }
-  
+
 }
 
 export let userService = new UserService();
 
 class OrgService{
-  
+
   getAllOrg(): Promise<Organization[]>{
     return axios.get(url + '/org');
   }
-  
+
   getOrgByID(id: number): Promise<Organization[]>{
     return axios.get(url + '/org/' + id);
   }
-  
+
   updateOrgByID(org: Organization): Promise<void>{
     return axios.put(url + '/org/' + org.org_id);
   }
-  
+
   deleteOrgByID(id: number): Promise<void>{
     return axios.delete(url + '/org/' + id);
   }
-  
+
   addNewOrg(org: Organization): Promise<void>{
     return axios.post(url + '/newOrg', org);
   }
-  
+
   getCountOrg(): Promise<number>{
     return axios.get(url + '/orgCount');
   }
-  
+
 }
 
 export let orgService = new OrgService();
 
 
 class EmployeeService {
-	
+
 	addEmployee(newemployee: Register): Promise<void> {
 		console.log("DATA TIL SERVICE: ", newemployee);
 		// console.log(axios.post(domain + '/admin/legginn', article, axiosConfig));
 		return axios.put(url + "/newuser", newemployee);
 	}
-	
-	
+
+
 }
 export let employeeService = new EmployeeService();
-<<<<<<< HEAD
 
 class MapService {
 
@@ -274,5 +273,3 @@ class MapService {
 }
 
 export let mapService = new MapService();
-=======
->>>>>>> 82be36808e502e51fbc3b2388caf9212d58a8b6e
