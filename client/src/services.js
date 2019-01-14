e058317a188afb8c4bbfb0f6d8a8689848cdc8f4
Mari Teiler-Johnsen <mari.teiler.johnsen@gmail.com>
2019-01-14 14:13:27 +0100

    testing promise object for verifying password

--- client/src/services.js
+++ client/src/services.js
@@ -262,15 +262,27 @@ class UserService {
 	 * @param updatePassword Includes variables {user_id, oldPassword, newPassword}
 	 * @returns {number} Returns 1 if verifying and change of password succeeds. Returns 0 if oldPassword is wrong
 	 */
-  verifyOldPasswordAndUpdatePWord(updatePassword: UserVerifyOldPWordAndUpdate): Promise<number>{
-  	const res = axios.get(url + '/userVerification', updatePassword);
-  	if(res === 1){
-  		axios.put(url + '/updateUserPword', {"user_id": updatePassword.user_id, "password": updatePassword.newpassword});
-			return 1;
-		}else{
-  		return 0;
+		verifyOldPasswordAndUpdatePWord (updatePassword: UserVerifyOldPWordAndUpdate): Promise<number> {
+			const res = axios.get(url + '/userVerification', updatePassword);
+			
+			res.then((result) => {
+				const promise = new Promise((resolve) => {
+					if (result) {
+						axios.put(url + '/updateUserPword', {
+							"user_id": updatePassword.user_id,
+							"password": updatePassword.newpassword
+						});
+						resolve(1);
+					} else {
+						resolve(0);
+					}
+					
+				});
+				return promise;
+			});
+			
+			
 		}
-	}
 
 }
 
