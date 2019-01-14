6114451a98d8ca36a7c917077097f9bea1605da5
IamAltair <mathibra@stud.ntnu.no>
2019-01-14 11:20:39 +0100

    Added map reference in index

--- client/src/index.js
+++ client/src/index.js
@@ -23,6 +23,7 @@ import Admin from "./components/Admin";
 import UserEdit from "./components/UserEdit";
 import ChangePassword from "./components/ChangePassword";
 import Map from "./components/Map";
+import MapContainer from "./components/ReportMap";
 
 
 const history = createHashHistory();
@@ -86,7 +87,7 @@ function renderRoot() {
             <Route exact path="/register" component={Register}/>
             <Route exact path="/nyansatt" component={NewEmployee}/>
             <Route exact path="/nypassord/:id" component={ChangePassword} />
-            <Route exact path="/map" component={Map} />
+            <Route exact path="/map" component={MapContainer} />
             <PrivateRoute path="/admin" component={Admin} />
           </div>
         </div>
