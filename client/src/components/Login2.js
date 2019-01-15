a30e79ffe1643506d418f5577fc3ef7e29c58a0f
erlinrol <erlinrol@stud.ntnu.no>
2019-01-15 09:44:21 +0100

    trying to merge

--- client/src/components/Login2.js
+++ client/src/components/Login2.js
@@ -1,24 +1,20 @@
-import React from 'react';
+// @flow
 
-import { authService } from '../authservices';
+import * as React from "react";
+import { Component } from "react-simplified";
+import { userService } from "../services";
 
-export class Login extends React.Component {
-    constructor(props) {
-        super(props);
+export default class Login extends Component {
+  login = [];
+  message = "";
 
-        userService.logout();
+  response = "";
 
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false,
-            loading: false,
-            error: ''
-        };
+  state = {
+    username: "",
+    password: ""
+  };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
 
   /*
 
@@ -114,65 +110,24 @@ w
       return null;
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
+    const login = {
+      username: this.state.username,
+      password: this.state.password
+    };
 
-        this.setState({ submitted: true });
-        const { username, password, returnUrl } = this.state;
-
-        // stop here if form is invalid
-        if (!(username && password)) {
-            return;
-        }
-
-        this.setState({ loading: true });
-        userService.login(username, password)
-            .then(
-                user => {
-                    const { from } = this.props.location.state || { from: { pathname: "/" } };
-                    this.props.history.push(from);
-                },
-                error => this.setState({ error, loading: false })
-            );
-    }
-
-    render() {
-        const { username, password, submitted, loading, error } = this.state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <div className="alert alert-info">
-                    Username: test<br />
-                    Password: test
-                </div>
-                <h2>Login</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                        {submitted && !username &&
-                        <div className="help-block">Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                        <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary" disabled={loading}>Login</button>
-                        {loading &&
-                        <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                    </div>
-                    {error &&
-                    <div className={'alert alert-danger'}>{error}</div>
-                    }
-                </form>
-            </div>
-        );
-    }
+    console.log(" ------ ");
+    this.message = "Login successful";
+    console.log("this login: ", login);
+    userService
+      .login(login)
+      .then(response => {
+        this.message = response.reply;
+        sessionStorage.setItem("storedtoken", response.jwt);
+        console.log("storedtoken: " + sessionStorage.getItem("storedtoken"));
+      })
+      .catch((error: Error) => {
+        if (error.message.includes("401"))
+          this.message = "Feil brukernavn eller passord.";
+      });
+  }
 }
-
-export { Login };
