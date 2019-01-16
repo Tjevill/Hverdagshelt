9295f352a4e7115b3fe131174d2f211d55fcbb4f
erlinrol <erlinrol@stud.ntnu.no>
2019-01-16 09:30:42 +0100

    Fixed validation redirect

--- client/src/components/ReportValidation.js
+++ client/src/components/ReportValidation.js
@@ -37,7 +37,7 @@ export default class ReportValidation extends Component {
     componentDidMount() {
         let timeLeftVar = this.secondsToTime(this.state.seconds);
         this.setState({ time: timeLeftVar });
-        setTimeout(() => (history.push("#/profile")), 5000);
+        setTimeout(() => (window.location = "#profile"), 5000);
     }
 
     startTimer() {
