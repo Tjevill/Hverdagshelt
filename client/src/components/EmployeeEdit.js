1d5f9c332cd57af242085a1fb89f2178278f4a23
erlinrol <erlinrol@stud.ntnu.no>
2019-01-18 10:01:06 +0100

    Employee edit

--- client/src/components/EmployeeEdit.js
+++ client/src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Router, NavLink } from "react-router-dom";
 import createHashHistory from "history/createHashHistory";
-import { employeeService } from "../services";
+import { userService, employeeService } from "../services";
 import { Alert, Card, ListGroup, Row, Column, Button, Form, Loading} from './widgets';
 
 const history = createHashHistory();
@@ -42,6 +42,15 @@ export default class EmployeeEdit extends Component {
         this.forceUpdate();
       })
       .catch((error: Error) => Alert.danger(error.message));
+
+      userService
+        .getDistricts()
+        .then(fylker => {
+            console.log(fylker);
+            this.forceUpdate();
+        })
+        .catch((error: Error) => Alert.danger(error.message));
+
   }
 
   /*<div className="jumbotron">
@@ -58,74 +67,63 @@ export default class EmployeeEdit extends Component {
           <div className="container text-center">
             <div className="row">
               <div className="col">
-           <div className="form-group">
-            Navn:{" "}
-            <input
-              className={"form-control"}
-              type="text"
-              name="name"
-              defaultValue={this.user.name}
-              onChange={event => (this.user.name = event.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            Mobil:{" "}
-            <input
-            className={"form-control"}
-              type="number"
-              defaultValue={this.user.tel}
-              name="tel"
-              onChange={event => {
-                if(event.target.value.length > 8) {
-                  event.target.value = (event.target.value-(event.target.value%10))/10;
-                }
-                this.user.tel = event.target.value;
-              }}
-            />
-          </div>
-          <div className="form-group">
-            Epost:{" "}
-            <input
-            className={"form-control"}
-              type="email"
-              defaultValue = {this.user.email}
-              name="zipcode"
-              onChange={event => (this.user.email = event.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            Kommune:{""}
-            <input
-            className={"form-control"}
-              type="text"
-              name="commune"
-              defaultValue={this.user.commune}
-              maxLength="8"
-              onChange={event => (this.user.commune = event.target.value)}
-            />
-            <div className="invalid-feedback">Ugydig telefon</div>
-          </div>
-          <div className="form-group">
-            Fylke:{" "}
-            <input
-              className={"form-control"}
-              type="text"
-              defaultValue={this.user.county}
-              name="county"
-              onChange={event => (this.user.county = event.target.value)}
-            />
-            <div className="invalid-feedback">Ugydig Email</div>
-            </div>
-            <Button.Success onClick={() => this.save()}>Lagre</Button.Success>
-            <Button.Light onClick={() => history.push('/admin/'+this.user.user_id)}>Avbryt</Button.Light>
-            </div>
-            <div className="col">
-            <br/><br/><br/><br/>
-              <img src={this.bilde} width="200"/>
+                <div className="form-group">
+                  Navn:{" "}
+                  <input
+                    className={"form-control"}
+                    type="text"
+                    name="name"
+                    defaultValue={this.user.name}
+                    onChange={event => (this.user.name = event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  Mobil:{" "}
+                  <input
+                    className={"form-control"}
+                    type="number"
+                    defaultValue={this.user.tel}
+                    name="tel"
+                    onChange={event => {
+                      if(event.target.value.length > 8) {
+                        event.target.value = (event.target.value-(event.target.value%10))/10;
+                      }
+                      this.user.tel = event.target.value;
+                    }}
+                  />
+                </div>
+                <div className="form-group">
+                  Epost:{" "}
+                  <input
+                    className={"form-control"}
+                    type="email"
+                    defaultValue = {this.user.email}
+                    name="zipcode"
+                    onChange={event => (this.user.email = event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                Kommune:{""}
+                  <input
+                    className={"form-control"}
+                    type="text"
+                    name="commune"
+                    defaultValue={this.user.commune}
+                    maxLength="8"
+                    onChange={event => (this.user.commune = event.target.value)}
+                  />
+                  <div className="invalid-feedback">Ugydig telefon</div>
+                </div>
+                <Button.Success onClick={() => this.save()}>Lagre</Button.Success>
+                <Button.Light onClick={() => history.push('/admin/'+this.user.user_id)}>Avbryt</Button.Light>
+              </div>
+              <div className="col">
+                <br/><br/><br/><br/>
+                <img src={this.bilde} width="200"/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       );
     } else {
       return (
