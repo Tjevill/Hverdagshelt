02b0258d0dd274ac07389746099f2bfed183ad61
IamAltair <mathibra@stud.ntnu.no>
2019-01-24 12:35:13 +0100

    added config to gitignore

--- client/src/index.js
+++ client/src/index.js
@@ -177,6 +177,7 @@ class Main extends Component {
                                 <Route exact path="/" component={forsideMain} />
                                 <Route path="/" component={ikkeforsideMain} />
                             </div>
+                            
 
                             {/*Main componenets*/}
                             <div>
