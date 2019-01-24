2e56a622e2e3184837cd3b6a5993cf391b4a5d5b
Øyvind Valstadsve <oyvinval@stud.ntnu.no>
2019-01-24 19:49:43 +0100

    Bug fixing part INFINITE INTEGER ERROR

--- client/src/components/IssueOverview.js
+++ client/src/components/IssueOverview.js
@@ -253,11 +253,19 @@ export default class IssueOverview extends Component<{
         <tbody>
           {this.cateside.map(casen => (
             <tr>
-              <th>{casen.case_id}</th>
+                <td className="clickable-link" onClick={() => history.push("/case/" + casen.case_id)}>{casen.case_id}</td>
               <td className="clickable-link" onClick={() => history.push("/case/" + casen.case_id)}>
                 {casen.headline}
               </td>
-              <td>{this.statusname[casen.status_id]}</td>
+                <td className="clickable-link" onClick={() => history.push("/case/" + casen.case_id)}>{this.statusname[casen.status_id]}</td>
+                <td><button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => { history.push("/case/" + casen.case_id)
+                    }}
+                >
+                    Se detaljer
+                </button></td>
             </tr>
           ))}
         </tbody>
@@ -284,123 +292,132 @@ export default class IssueOverview extends Component<{
 
     if (this.loaded) {
       return (
-        <>
-          <div className="jumbotron">
-            <div className="container text-center">
-              <div className="btn-group" role="group" aria-label="First group">
-                <button type="button" className="btn btn-info col-sm-3"  onClick={()=>this.category(0,"All")}>
-                  All
-                </button>
-                {this.categories.map(category => (
-                  <button type="button" className="btn btn-info col-sm-3" onClick={()=>this.category(category.category_id,category.description)}>
-                    {category.description}
-                  </button>
-                ))}
-              </div>
-              <br />
-              <br />
-              <div className="form-row">
-                <div className="form-group col-4">
-                  <label for="inputFylke">Velg Fylke</label>
-                  <select
-                    id="fylke"
-                    name="fylke"
-                    className="form-control"
-                    onChange={this.handleChangeFylke}
-                  >
-                    <option selected value={0}>
-                      Alle{" "}
-                    </option>
-                    {this.fylker.map(fylke => {
-                      return <option value={fylke.ID}>{fylke.navn}</option>;
-                    })}
-                  </select>
-                </div>
-                <div className="form-group col-4">
-                  <label for="inputKommune">Velg Kommune</label>
-                  <select
-                    id="kommune"
-                    name="kommune"
-                    className="form-control"
-                    onChange={this.handleChangeKommune}
-                  >
-                    <option selected> Velg fylke først </option>
-                    {this.kommuner.map(kommune => {
-                      return (
-                        <option value={kommune.Name}>{kommune.navn}</option>
-                      );
-                    })}
-                  </select>
-                </div>
-                <div className="form-group col-4">
-                  <label for="inputKommune">Velg Status</label>
-                  <select
-                    id="status"
-                    name="status"
-                    className="form-control"
-                    onChange={this.handleChangeStatus}
-                  >
-                    <option value={0}>Alle</option>
-                    <option value={1}>Registrert</option>
-                    <option value={2}>Under Vurdering</option>
-                    <option value={3}>Satt på vent</option>
-                    <option value={4}>Arbeid pågår</option>
-                    <option value={6}>Løst</option>
-                  </select>
-                </div>
-              </div>
-              {this.Meldning}
-            </div>
-          </div>
+        <div>
 
-          <div className="container">
             <div className="row">
-              <div className="col">
-                <h2 className="display-4" id="Saker-tittel">Saker</h2>
-              </div>
-              <div className="col" />
-              <div className="col" />
-              <div className="col">
-                <span className="glyphicon glyphicon-search" aria-hidden="true" />
-                <input
-                  type="text"
-                  id="search"
-                  name="search"
-                  placeholder="Søk tittel.."
-                  onChange={this.search}
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <p>Kategori:{this.categoryname} &nbsp; Status:{this.statusname[this.statusid]} &nbsp; Kommune: {this.kommune}&nbsp;</p>
-              </div>
-            </div>
-            <Router history={history}>
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th scope="col">Saksnummer</th>
-                    <th scope="col">Tittel</th>
-                    <th scope="col">Nåværende status</th>
-                  </tr>
-                </thead>
-                {lists}
-              </table>
-            </Router>
-            <br />
-            <br />
-          </div>
+                <div className="col-md-1"/>
+                <div className="col-md-10">
 
-          <div id="toolbar">
-            <div className="wrapper text-center">
-              <div className="btn-group">{sidebuttons}</div>
+
+                  <div className="container">
+                    <div className="">
+                      <div className="btn-group" role="group" aria-label="First group">
+                        <button type="button" className="btn btn-info buttonpadding"  onClick={()=>this.category(0,"All")}>
+                          All
+                        </button>
+
+                        {this.categories.map(category => (
+                          <><button type="button" className="btn btn-info buttonpadding" onClick={()=>this.category(category.category_id,category.description)}>
+                            {category.description}
+                          </button></>
+                        ))}
+
+                      </div>
+                      <br />
+                      <br />
+                      <div className="form-row">
+                        <div className="form-group col-4">
+                          <label for="inputFylke">Velg Fylke</label>
+                          <select
+                            id="fylke"
+                            name="fylke"
+                            className="form-control"
+                            onChange={this.handleChangeFylke}
+                          >
+                            <option selected value={0}>
+                              Alle{" "}
+                            </option>
+                            {this.fylker.map(fylke => {
+                              return <option value={fylke.ID}>{fylke.navn}</option>;
+                            })}
+                          </select>
+                        </div>
+                        <div className="form-group col-4">
+                          <label for="inputKommune">Velg Kommune</label>
+                          <select
+                            id="kommune"
+                            name="kommune"
+                            className="form-control"
+                            onChange={this.handleChangeKommune}
+                          >
+                            <option selected> Velg fylke først </option>
+                            {this.kommuner.map(kommune => {
+                              return (
+                                <option value={kommune.Name}>{kommune.navn}</option>
+                              );
+                            })}
+                          </select>
+                        </div>
+                        <div className="form-group col-4">
+                          <label for="inputKommune">Velg Status</label>
+                          <select
+                            id="status"
+                            name="status"
+                            className="form-control"
+                            onChange={this.handleChangeStatus}
+                          >
+                            <option value={0}>Alle</option>
+                            <option value={1}>Registrert</option>
+                            <option value={2}>Under Vurdering</option>
+                            <option value={3}>Satt på vent</option>
+                            <option value={4}>Arbeid pågår</option>
+                            <option value={6}>Løst</option>
+                          </select>
+                        </div>
+                      </div>
+                      {this.Meldning}
+                    </div>
+
+                      <div className="container">
+                          <div className="row">
+                              <div className="col">
+                                  <h2 className="display-4" id="Saker-tittel">Saker</h2>
+                              </div>
+                              <div className="col" />
+                              <div className="col" />
+                              <div className="col">
+                                  <span className="glyphicon glyphicon-search" aria-hidden="true" />
+                                  <input
+                                      type="text"
+                                      id="search"
+                                      name="search"
+                                      placeholder="Søk tittel.."
+                                      onChange={this.search}
+                                  />
+                              </div>
+                          </div>
+                          <div className="row">
+                              <div className="col">
+                                  <p>Kategori:{this.categoryname} &nbsp; Status:{this.statusname[this.statusid]} &nbsp; Kommune: {this.kommune}&nbsp;</p>
+                              </div>
+                          </div>
+                          <Router history={history}>
+                              <table className="table table-hover">
+                                  <thead>
+                                  <tr>
+                                      <th scope="col">Nr.</th>
+                                      <th scope="col">Tittel</th>
+                                      <th scope="col">Nåværende status</th>
+                                      <th scope="col">&nbsp;</th>
+                                  </tr>
+                                  </thead>
+                                  {lists}
+                              </table>
+                          </Router>
+
+                      </div>
+
+                      <div id="toolbar">
+                          <div className=" text-center">
+                              <div className="btn-group">{sidebuttons}</div>
+                          </div>
+                      </div>
+                  </div>
+
+                </div>
+                <div className="col-md-1"/>
             </div>
-          </div>
-
-          <br/><br/>
-
-        </>
+        </div>
       );
     } else {
       return <Loading />;
