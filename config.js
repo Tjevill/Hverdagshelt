90095f7b262e4b472f817ca0628af943b01519fe
Mari Teiler-Johnsen <mari.teiler.johnsen@gmail.com>
2019-01-24 11:19:52 +0100

    jest testing and config file for sensitive data

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+	email: {
+		user: 'bedrehverdagshelt@gmail.com',
+		password: 'JegErDinHelt69'
+	},
+	db: {
+		host: 'mysql.stud.iie.ntnu.no',
+		user: 'mariteil',
+		database: 'mariteil',
+		password: 'Fs7ABKyd'
+	},
+	testdb: {
+		host: 'mysql.stud.iie.ntnu.no',
+		user: 'benos',
+		database: 'benos',
+		password: 'uJHtIkcl'
+	}
+};
+
+module.exports = config;
--- server/tests/categoryTest.js
+++ server/tests/categoryTest.js
@@ -2,17 +2,23 @@
 
 const mysql = require('mysql');
 jest.setTimeout(50000);
+const config = require('../../config.js');
 
 const CategoryDao = require("../dao/categorydao.js");
 const runsqlfile = require('./runsqlfile.js');
 
+const host = config.testdb.host;
+const user = config.testdb.user;
+const database = config.testdb.database;
+const password = config.testdb.password;
+
 // GitLab CI Pool
-const pool = mysql.createPool({
+let pool = mysql.createPool({
 	connectionLimit: 1,
-	host: 'mysql.stud.iie.ntnu.no',
-	user: 'benos',
-	password: 'uJHtIkcl',
-	database: 'benos',
+	host: host,
+	user: user,
+	password: password,
+	database: database,
 	debug: false,
 	multipleStatements: true
 });
